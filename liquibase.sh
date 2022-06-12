dd0b2a1885fdb124bebedca0ca3d36f3dc877b44
Calvine Otieno <nyarangaotieno@gmail.com>
2022-06-12 21:02:13 +0300

    chore: update the credentials file
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- liquibase.sh
+++ liquibase.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./cloud_sql_proxy -dir=/cloudsql -instances=${DB_PROJECT}:${DB_REGION}:${DB_INSTANCE}=tcp:3306 -credential_file=/secrets/cloudsql/cloud_sql_credentials.json &
+./cloud_sql_proxy -dir=/cloudsql -instances=${DB_PROJECT}:${DB_REGION}:${DB_INSTANCE}=tcp:3306 -credential_file=/secrets/cloudsql/credentials.json &
 sleep 10
 
 docker-entrypoint.sh --url=${DB_URL} --username=${DB_USERNAME} --password=${DB_PASSWORD} --contexts=production --classpath=/liquibase/changelog --changeLogFile=db-changelog-master.mysql.xml update
